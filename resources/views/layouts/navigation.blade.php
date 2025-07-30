d27f9976614e015b5494b18a05916cc357e0af2d
Moeed Anwar <moeedanwar28@gmail.com>
2025-07-30 23:13:55 +0500

    added mobile nav menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('exp.index')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('exp.index')" :active="request()->routeIs('exp.index')">
                         {{ __('Expense Tracker') }}
                     </x-nav-link>
                 </div>
@@ -74,6 +74,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('exp.index')" :active="request()->routeIs('exp.index')">
+                {{ __('Expense Tracker') }}
             </x-responsive-nav-link>
         </div>
 
